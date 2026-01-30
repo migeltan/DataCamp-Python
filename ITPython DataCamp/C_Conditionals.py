789ea118feefb9bb68ae194dd9cbc6f9bc82167f
miggy <migellltan@gmail.com>
2026-01-31 01:36:54 +0800

    condi

---
+++ ITPython DataCamp/C_Conditionals.py
@@ -0,0 +1,42 @@
+#conditionals
+#if, elif, else
+
+'''
+syntax: depends on indents
+
+if condition: 
+    expression
+    
+if condition:
+    expression
+elif condition:
+    expression
+else:
+    expression
+
+'''
+z = 5
+if z%2==0:
+    print('z is even')
+else:
+    print('z is odd')
+
+x = 6
+if x % 2 ==0 :
+    print('x is divi by 2')
+elif x % 3  == 0 :
+    print('x is divi by 3')
+else:
+    print('x is neither 2 or 3 divi')
+
+#examples:
+# Define variables
+room = "kit"
+area = 14.0
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+# if statement for area
+if area>15:
+    print('big place!')
+    
