d6dd9483cd62463b0bbd58cd2ef9275ad2be428a
miggy <migellltan@gmail.com>
2026-02-27 00:15:31 +0800

    Laboratory Lesson #1

---
+++ IPT/L1_LABS.py
@@ -0,0 +1,47 @@
+print("hello world")
+#function in python is def
+#input is scanf
+'''
+scanf:
+num = input ("Enter a number: " )
+print("You Entered: ", num)
+print("Data type of num: ", type(num))
+'''
+
+#typecasting, transforms the input into a new data type
+num1 = int (input("Enter a number: "))
+      
+#conditionals:
+print(num1)
+if num1 == 0:
+    print(num1, "neutral.")
+elif num1 < 0:
+    print(num1, "is a negative number.")
+else:
+    print(num1, "is a positive number.")
+
+#loop:
+#i is E1
+#1 is 
+'''
+for i in num1:
+    print("")
+  '''  
+#while loop
+i = 0
+while (i < 3):
+    i = i + 1
+    print ("kkkk")
+else:
+    print("butaw")
+    
+#for loop:
+'''
+for iterator_var in sequence:
+    statements
+    #python program
+    #iterate over range 0 to n-1
+'''
+n = 4
+for i in range(0, n):
+    print(i)
