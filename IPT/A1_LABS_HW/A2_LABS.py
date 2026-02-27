783177e48fe98f884b982e88313f00dbe229bc4c
miggy <migellltan@gmail.com>
2026-02-27 22:07:39 +0800

    assignment #2 - ascii values

---
+++ IPT/A1_LABS_HW/A2_LABS.py
@@ -0,0 +1,14 @@
+#Assignment #2: Write a program to print all ASCII character with their values.
+#A-Z, a-z, 0-9
+
+print("Uppercase Letters (A-Z): ")
+for char in range(ord('A'), ord('Z') + 1):
+    print(chr(char), "=", char)
+
+print("\nLowercase Letters (a-z): ")
+for char in range(ord('a'), ord('z') + 1):
+    print(chr(char), "=", char)
+
+print("\nDigits (0-9): ")
+for char in range(ord('0'), ord('9') + 1):
+    print(chr(char), "=", char)
