3a473ebd028c41c55e432926f6ad88ce579e6547
miggy <migellltan@gmail.com>
2026-02-27 16:43:16 +0800

    Assignment #1 in IPT: if-else

---
+++ IPT/A1_LABS_HW/A1_LABS.py
@@ -0,0 +1,41 @@
+#Assignment #1: Write a program to enter a number and print it in words.
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print ("Invalid, non-negative number!")
+elif num == 1:
+    print ("You entered: " + str(num))
+    print ("One")
+elif num == 2:
+    print ("You entered: " + str(num))
+    print ("Two")
+elif num == 3:
+    print ("You entered: " + str(num))
+    print ("Three")
+elif num == 4:
+    print ("You entered: " + str(num))
+    print ("Four")
+elif num == 5:
+    print ("You entered: " + str(num))
+    print ("Five")
+elif num == 6:
+    print ("You entered: " + str(num))
+    print ("Six")
+elif num == 7:
+    print ("You entered: " + str(num))
+    print ("Seven")
+elif num == 8:
+    print ("You entered: " + str(num))
+    print ("Eight")
+elif num == 9:
+    print ("You entered: " + str(num))
+    print ("Nine")
+elif num == 10:
+    print ("You entered: " + str(num))
+    print ("Ten")
+else:
+    print ("Invalid, enter a number between 1-10!")
+    
+
+    
