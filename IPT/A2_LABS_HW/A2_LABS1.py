c671cac7775a077a594982f9e38e5efe71a99d7c
miggy <migellltan@gmail.com>
2026-03-16 18:47:29 +0800

    changes abt description of code

--- IPT/A2_LABS_HW/A2_LABS1.py
+++ IPT/A2_LABS_HW/A2_LABS1.py
@@ -1,6 +1,10 @@
+''' In keyword arguments, values are passed by explicitly 
+specifying the parameter names, so the order doesn't matter. '''
+
 def my_function(animal, name):
   print("I have a", animal)
   print("My", animal + "'s name is", name)
 
+            #actual pass via keywords
 my_function(animal = "dog", name = "Buddy")
 #keyword arguments
